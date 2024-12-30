336770d5a47b68bdb9af73f9b0bc1ddd7e53db7a
Huo <huo9190@163.com>
2024-12-30 15:46:16 +0800

    更新文档

--- game/component/mj/game_test.go
+++ game/component/mj/game_test.go
@@ -11,3 +11,14 @@ func TestDel(t *testing.T) {
 	cards := gf.delCardFromArray([]mp.CardID{1, 2, 11, 3, 4, 5, 6, 11, 7, 8, 9, 10}, 11, 2)
 	fmt.Println(cards)
 }
+
+var cur int
+
+func TestAppend(t *testing.T) {
+	old := make([][]mp.CardID, 4)
+	old[0] = []mp.CardID{1, 2, 3, 6, 7, 8, 9, 0, 10, 11, 11, 12, 13}
+	old[1] = []mp.CardID{4, 2, 5, 6, 11, 8, 9, 0, 10, 23, 34, 56, 78}
+	cur = 0
+	old[cur] = append(old[cur], 9)
+	fmt.Println(old)
+}
